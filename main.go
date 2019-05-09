e4acf650a5925ca92dc8243f8e372a4bdb458c7e
Tamal Saha <tamal@appscode.com>
2019-05-08 23:34:03 -0700

    added-all

--- main.go
+++ main.go
@@ -91,11 +91,11 @@ gomod-tools github.com/appscode/voyager
 
 	glideFile := filepath.Join(dir, "glide.yaml")
 	if Exists(glideFile) {
-		err := sh.Command("glide", "slow").Run()
-		if err != nil {
-			fmt.Println(err)
-			// continue
-		}
+		// err := sh.Command("glide", "slow").Run()
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	// continue
+		// }
 
 		data, err := ioutil.ReadFile(glideFile)
 		if err != nil {
@@ -151,18 +151,18 @@ gomod-tools github.com/appscode/voyager
 	if Exists(depFile) {
 		fmt.Println("found Gopkg.toml: ", depFile)
 
-		err := sh.Command("dep", "ensure", "-update").Run()
-		if err != nil {
-			fmt.Println(err)
-			// continue
-		}
+		// err := sh.Command("dep", "ensure", "-update").Run()
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	// continue
+		// }
 
 		var cfg Dep
 		if _, err := toml.DecodeFile(depFile, &cfg); err != nil {
 			log.Fatalln(err)
 		}
 
-		err = sh.Command("go", "mod", "init").Run()
+		err := sh.Command("go", "mod", "init").Run()
 		if err != nil {
 			fmt.Println(err)
 		}
